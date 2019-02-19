8aba756882cef4782817867ba51d15738d92757d
JuanRubiio <juan12_rubio@hotmail.com>
2019-02-19 23:27:44 +0100

    servicios de actor, administrator y configuration

---
+++ src/main/java/services/AdministratorService.java
@@ -0,0 +1,102 @@
+
+package services;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import repositories.AdministratorRepository;
+import security.Authority;
+import security.UserAccount;
+import domain.Administrator;
+
+@Service
+@Transactional
+public class AdministratorService {
+
+	@Autowired
+	private AdministratorRepository	administratorRepository;
+
+
+	//	@Autowired
+	//private MessageBoxService		messageBoxService;
+
+	public Administrator create() {
+		Administrator res;
+
+		res = new Administrator();
+		final UserAccount userAccount = new UserAccount();
+		final List<Authority> authorities = new ArrayList<Authority>();
+		final Authority authority = new Authority();
+		authority.setAuthority(Authority.ADMIN);
+		authorities.add(authority);
+		userAccount.setAuthorities(authorities);
+		Assert.notNull(res);
+		res.setUserAccount(userAccount);
+		//this.messageBoxService.addDefaultMessageBoxs(res);
+
+		return res;
+	}
+
+	public Administrator save(final Administrator administrator) {
+		Administrator res;
+		Assert.notNull(administrator);
+		res = this.administratorRepository.save(administrator);
+		Assert.notNull(res);
+		return res;
+	}
+
+	public Collection<Administrator> findAll() {
+		Collection<Administrator> res;
+
+		res = this.administratorRepository.findAll();
+
+		Assert.notNull(res);
+
+		return res;
+	}
+
+	public Administrator findOne(final Integer administratorId) {
+		Administrator res;
+
+		Assert.notNull(administratorId);
+
+		res = this.administratorRepository.findOne(administratorId);
+
+		Assert.notNull(res);
+		return res;
+	}
+
+	//Queries del dashboard
+	public List<Object> query1() {
+		return this.administratorRepository.query1();
+	}
+	public List<Object> query2() {
+		return this.administratorRepository.query2();
+	}
+	public List<Object> query3() {
+		return this.administratorRepository.query3();
+	}
+	public List<Object> query4() {
+		return this.administratorRepository.query4();
+	}
+	public Double query5() {
+		return this.administratorRepository.query5();
+	}
+	public Double query6() {
+		return this.administratorRepository.query6();
+	}
+	public Double query7() {
+		return this.administratorRepository.query7();
+	}
+	public Double query8() {
+		return this.administratorRepository.query8();
+	}
+
+}

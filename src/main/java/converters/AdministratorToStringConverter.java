2b45b4e4abbb2b93d8243d79842b926bc5033382
JuanRubiio <juan12_rubio@hotmail.com>
2019-02-19 23:29:27 +0100

    converters de actor, administrator y configuration

---
+++ src/main/java/converters/AdministratorToStringConverter.java
@@ -0,0 +1,25 @@
+
+package converters;
+
+import javax.transaction.Transactional;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import domain.Administrator;
+
+@Component
+@Transactional
+public class AdministratorToStringConverter implements Converter<Administrator, String> {
+
+	@Override
+	public String convert(final Administrator arg0) {
+		String result;
+		if (arg0 == null)
+			result = null;
+		else
+			result = String.valueOf(arg0.getId());
+		return result;
+	}
+
+}
